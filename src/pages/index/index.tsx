8978dc55d5d7f6f525f59dfbc6d719b2ea71cf16
niying <niying@meican.com>
2024-06-19 10:56:39 +0800

    feat: 1.0.8-fix.npm.5

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,24 @@
-import { View, Text } from "@tarojs/components";
+import { View, Text, Lottie } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 // import { functionA } from "./test.js";
-import { func } from "lottiecomp";
+// import { func } from "lottiecomp";
+import LottieWarp, { version1 } from "lottiecomp";
 import "./index.scss";
 
 // functionA();
 export default function Index() {
   useLoad(() => {
     console.log("Page loaded.");
-    console.log(func(true)());
+    // console.log(func(true)());
+    console.log(version1);
   });
 
   return (
     <View className="index">
       <Text>Hello world!</Text>
+      <LottieWarp />
     </View>
   );
 }
+
+Lottie;
